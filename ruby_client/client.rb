cb49d751b3445d508c6f1c337d86e3b689a6d907
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-08-07 20:37:34 -0300

    Accept arguments to start Ruby client

--- ruby_client/client.rb
+++ ruby_client/client.rb
@@ -4,6 +4,6 @@ require_relative 'syrinx_services_pb'
 require_relative 'broadcast_client'
 require_relative 'tune_client'
 
-SyrinxClient::Broadcast.new.perform
-# SyrinxClient::Tune.new.perform
+SyrinxClient::Broadcast.new.perform if ARGV.first == '--broadcast'
+SyrinxClient::Tune.new.perform if ARGV.first == '--tune'
 
