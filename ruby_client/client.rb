a57ee02de8ae09122dac9b32f1cfc2cbbf1e6953
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-09-01 12:23:19 -0300

    Require Ruby CrudeLogger

--- ruby_client/client.rb
+++ ruby_client/client.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 require_relative 'syrinx_services_pb'
 require_relative 'broadcast_client'
 require_relative 'tune_client'
+require_relative 'crude_logger'
 
 SyrinxClient::Broadcast.new.perform if ARGV.first == '--broadcast'
 SyrinxClient::Tune.new.perform if ARGV.first == '--tune'
